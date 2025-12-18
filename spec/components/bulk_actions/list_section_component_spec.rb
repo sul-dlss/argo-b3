ce0fa427a1734d16c7dbf733e09da49705c99507
Justin Littman <justinlittman@stanford.edu>
2025-12-18 17:03:09 -0500

    Only link implemented bulk actions.
    refs #78

--- spec/components/bulk_actions/list_section_component_spec.rb
+++ spec/components/bulk_actions/list_section_component_spec.rb
@@ -5,16 +5,28 @@ require 'rails_helper'
 RSpec.describe BulkActions::ListSectionComponent, type: :component do
   let(:component) do
     described_class.new(label: 'My bulk actions', classes: 'my-class',
-                        bulk_action_configs: [BulkActions::MANAGE_RELEASE])
+                        bulk_action_configs: [BulkActions::REINDEX, test_bulk_action_config])
+  end
+
+  let(:test_bulk_action_config) do
+    # Does not have a path helper to test that case
+    BulkActions::Config.new(
+      label: 'Test',
+      help_text: 'This is a test bulk action.'
+    )
   end
 
   it 'renders the section' do
     render_inline(component)
 
     expect(page).to have_css('section.my-class#my-bulk-actions-bulk-actions-section h2', text: 'My bulk actions')
-    within('section ul li:nth-of-type(1)') do
-      expect(page).to have_link('Manage release', href: '/bulk_actions/manage_release/new')
-      expect(page).to have_css('p', text: 'Adds release tags to individual objects.')
-    end
+    reindex_li = page.find('section ul li:nth-of-type(1)')
+    expect(reindex_li).to have_link('Reindex', href: '/bulk_actions/reindex/new')
+    expect(reindex_li).to have_css('p', text: 'Reindexes the DOR object in Solr.')
+
+    test_li = page.find('section ul li:nth-of-type(2)')
+    expect(test_li).to have_css('span', text: 'Test')
+    expect(test_li).to have_no_link('Test')
+    expect(test_li).to have_css('p', text: 'This is a test bulk action.')
   end
 end
