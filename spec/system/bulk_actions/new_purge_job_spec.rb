154f70d89ecde034ac2cfa0687b2b53c3778182f
Justin Littman <justinlittman@stanford.edu>
2026-01-09 17:39:06 -0500

    Adds purge bulk actions.
    refs #78

---
+++ spec/system/bulk_actions/new_purge_job_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Create a new purge bulk action' do
+  let!(:user) { create(:user) }
+
+  let(:druids) do
+    ['druid:pj757vx3102', 'druid:rt276nw8963']
+  end
+
+  let(:bulk_action_label) { BulkActions::PURGE.label }
+
+  before do
+    sign_in user
+  end
+
+  # NOTE: Not testing using last search or druids validation since already tested by new_reindex_spec.
+
+  context 'when a list of druids is provided' do
+    it 'submits a purge bulk action' do
+      visit new_bulk_action_path
+
+      click_link bulk_action_label
+
+      expect(page).to have_css('h1', text: bulk_action_label)
+      fill_in 'Enter druid list', with: druids.join("\n")
+
+      fill_in 'Describe this bulk action', with: 'Purge test items'
+
+      click_button 'Submit'
+
+      expect(page).to have_current_path(bulk_actions_path)
+
+      expect(page).to have_toast("#{bulk_action_label} submitted")
+
+      bulk_action = BulkAction.last
+      expect(bulk_action.action_type).to eq(BulkActions::PURGE.action_type.to_s)
+      expect(bulk_action.description).to eq('Purge test items')
+      expect(bulk_action.user).to eq(user)
+      expect(bulk_action.queued?).to be true
+
+      expect(BulkActions::PurgeJob).to have_been_enqueued.with(druids:, bulk_action:)
+    end
+  end
+end
