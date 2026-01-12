c8077dc23699bff4a403c7a51c2a2e7b9706bff0
Michael J. Giarlo <mjgiarlo@stanford.edu>
2026-01-12 15:17:13 -0800

    Rework bulk action job specs to use let variables instead of constants
    To avoid the annoying warnings about a constant being defined more than once.

--- spec/jobs/bulk_actions/bulk_action_job_spec.rb
+++ spec/jobs/bulk_actions/bulk_action_job_spec.rb
@@ -2,13 +2,10 @@
 
 require 'rails_helper'
 
-DRUID1 = 'druid:bb111cc2222'
-DRUID2 = 'druid:cc111dd2222'
-
 RSpec.describe BulkActions::BulkActionJob do
   # Setting count fields to ensure that they are reset.
   let(:bulk_action) { create(:bulk_action, druid_count_success: 100, druid_count_fail: 100, druid_count_total: 100) }
-
+  let(:druids) { %w[druid:bb111cc2222 druid:cc111dd2222] }
   let(:log) { instance_double(File, puts: nil, close: true) }
   let(:export_file) { instance_double(File, close: true) }
 
@@ -29,8 +26,8 @@ RSpec.describe BulkActions::BulkActionJob do
     before do
       bulk_action_item_class = Class.new(BulkActions::BulkActionJobItem) do
         def perform
-          success!(message: 'Testing successful') if druid == DRUID1
-          failure!(message: 'Testing failed') if druid == DRUID2
+          success!(message: 'Testing successful') if druid == 'druid:bb111cc2222'
+          failure!(message: 'Testing failed') if druid == 'druid:cc111dd2222'
         end
       end
       stub_const('TestBulkActionJob::TestBulkActionJobItem', bulk_action_item_class)
@@ -39,17 +36,17 @@ RSpec.describe BulkActions::BulkActionJob do
     end
 
     it 'performs the job' do
-      TestBulkActionJob.perform_now(bulk_action:, druids: [DRUID1, DRUID2])
+      TestBulkActionJob.perform_now(bulk_action:, druids:)
 
-      expect(TestBulkActionJob::TestBulkActionJobItem).to have_received(:new).with(druid: DRUID1, index: 0,
+      expect(TestBulkActionJob::TestBulkActionJobItem).to have_received(:new).with(druid: druids.first, index: 0,
                                                                                    job: instance_of(TestBulkActionJob))
-      expect(TestBulkActionJob::TestBulkActionJobItem).to have_received(:new).with(druid: DRUID2, index: 1,
+      expect(TestBulkActionJob::TestBulkActionJobItem).to have_received(:new).with(druid: druids.second, index: 1,
                                                                                    job: instance_of(TestBulkActionJob))
 
       expect(log).to have_received(:puts).with(/Starting TestBulkActionJob for BulkAction #{bulk_action.id}/)
       expect(log).to have_received(:puts).with(/Finished TestBulkActionJob for BulkAction #{bulk_action.id}/)
-      expect(log).to have_received(:puts).with(/Testing successful for #{DRUID1}/o)
-      expect(log).to have_received(:puts).with(/Testing failed for #{DRUID2}/o)
+      expect(log).to have_received(:puts).with(/Testing successful for #{druids.first}/o)
+      expect(log).to have_received(:puts).with(/Testing failed for #{druids.second}/o)
       expect(log).to have_received(:close)
       expect(export_file).to have_received(:close)
 
@@ -70,8 +67,8 @@ RSpec.describe BulkActions::BulkActionJob do
     before do
       bulk_action_item_class = Class.new(BulkActions::BulkActionJobItem) do
         def perform
-          success!(message: 'Testing successful') if druid == DRUID1
-          raise StandardError, 'Something bad happened' if druid == DRUID2
+          success!(message: 'Testing successful') if druid == 'druid:bb111cc2222'
+          raise StandardError, 'Something bad happened' if druid == 'druid:cc111dd2222'
         end
       end
       stub_const('TestBulkActionJob::TestBulkActionJobItem', bulk_action_item_class)
@@ -80,10 +77,10 @@ RSpec.describe BulkActions::BulkActionJob do
     end
 
     it 'performs the job' do
-      TestBulkActionJob.perform_now(bulk_action:, druids: [DRUID1, DRUID2])
+      TestBulkActionJob.perform_now(bulk_action:, druids:)
 
-      expect(log).to have_received(:puts).with(/Testing successful for #{DRUID1}/o)
-      expect(log).to have_received(:puts).with(/Failed StandardError Something bad happened for #{DRUID2}/o)
+      expect(log).to have_received(:puts).with(/Testing successful for #{druids.first}/o)
+      expect(log).to have_received(:puts).with(/Failed StandardError Something bad happened for #{druids.second}/o)
 
       expect(bulk_action.reload.druid_count_total).to eq(2)
       expect(bulk_action.druid_count_success).to eq(1)
