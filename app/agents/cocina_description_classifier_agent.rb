395bce900492c0eeb244fcaf4cee3c77097e98fe
Justin Littman <justinlittman@stanford.edu>
2026-05-04 15:26:59 -0400

    Switched models

--- app/agents/cocina_description_classifier_agent.rb
+++ app/agents/cocina_description_classifier_agent.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
+# Agent that determines the Cocina field that is being updated based on the user's request.
 class CocinaDescriptionClassifierAgent < RubyLLM::Agent
-  # model 'gemini-flash-latest'
   model 'gemini-3.1-flash-lite-preview', provider: Rails.env.production? ? :vertexai : :gemini
 
   instructions
@@ -11,4 +11,5 @@ class CocinaDescriptionClassifierAgent < RubyLLM::Agent
                     relatedResource adminMetadata marcEncodedData valueAt none]
   end
   thinking effort: :low
+  temperature 1.0 # With Gemini 3+, recommended to keep >= 1.0
 end
--- app/agents/cocina_description_editor_agent.rb
+++ app/agents/cocina_description_editor_agent.rb
@@ -1,29 +1,12 @@
 # frozen_string_literal: true
 
+# Agent that updates a Cocina description based on the user's request.
 class CocinaDescriptionEditorAgent < RubyLLM::Agent
-  # model 'gemini-3.1-pro-preview'
-  # model 'gemini-flash-latest'
-  model 'gemini-3-flash-preview', provider: Rails.env.production? ? :vertexai : :gemini
+  # model 'gemini-3-flash-preview', provider: Rails.env.production? ? :vertexai : :gemini
+  model 'gemini-3.1-flash-lite-preview', provider: Rails.env.production? ? :vertexai : :gemini
   chat_model Chat
-  # inputs :original_cocina_description_hash
-  # instructions do
-  #   [
-  #     prompt('instructions'),
-  #     prompt('common')
-  #   ].join("\n\n")
-  # end
-  # NOTE: schema isn't working correctly with Gemini, so requesting a JSON response.
-  # The response is described in the instructions.
-  # JSON response doesn't work with tools, so validating response separately.
-  # instructions cocina_description_hash: -> { cocina_description_hash }, property: -> { property }
-  # tools CocinaDescriptionValidatorTool
-  # tools do
-  #   CocinaDescriptionValidatorTool.new(original_cocina_description_hash:)
-  # end
   tools UrlFetchTool, ModelTool
   params generationConfig: { responseMimeType: 'application/json' }
-  # schema({ name: 'Cocina Description Schema', schema: CocinaDescriptionSchemaGenerator.call })
-  # schema CocinaDescriptionEditorSchema
   temperature 1.0 # With Gemini 3+, recommended to keep >= 1.0
-  thinking effort: :low
+  thinking effort: :medium
 end
