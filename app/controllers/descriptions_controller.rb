ce211a75ec74bf05a52030c6116c163e25c216cb
Ed Summers <ehs@pobox.com>
2026-05-12 08:47:56 -0400

    Make all JSON editable

--- app/controllers/descriptions_controller.rb
+++ app/controllers/descriptions_controller.rb
@@ -4,11 +4,58 @@
 class DescriptionsController < ApplicationController
   before_action :load_cocina_object
 
+  FIELD_RESULT_KEY = {
+    'title' => :title, 'note' => :note, 'language' => :language,
+    'contributor' => :contributor, 'subject' => :subject, 'form' => :form,
+    'event' => :event, 'related_resource' => :relatedResource
+  }.freeze
+
+  FIELD_MODEL_CLASS = {
+    'title' => Cocina::Models::Title,
+    'note' => Cocina::Models::DescriptiveValue,
+    'language' => Cocina::Models::Language,
+    'contributor' => Cocina::Models::Contributor,
+    'subject' => Cocina::Models::DescriptiveValue,
+    'form' => Cocina::Models::DescriptiveValue,
+    'event' => Cocina::Models::Event,
+    'related_resource' => Cocina::Models::RelatedResource
+  }.freeze
+
   def edit
     authorize! @cocina_object, with: ObjectPolicy, to: :edit_description?
     open_version_if_needed!
   rescue Sdr::Repository::Error
     # If pre-opening fails, save will attempt it again
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def field_json
+    authorize! @cocina_object, with: ObjectPolicy, to: :edit_description?
+
+    result_key = FIELD_RESULT_KEY[params.require(:field_type)]
+    return head :bad_request unless result_key
+
+    built = DescriptionBuilder.new(existing_description: {}).build(description_params)
+    item = built[result_key]&.first
+    return head(:unprocessable_content) unless item
+
+    render json: FIELD_MODEL_CLASS[params[:field_type]].new(item).to_h
+  rescue ActionController::ParameterMissing, JSON::ParserError,
+         Cocina::Models::ValidationError, Dry::Struct::Error
+    head :unprocessable_content
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def render_field
+    authorize! @cocina_object, with: ObjectPolicy, to: :edit_description?
+
+    data = JSON.parse(params.require(:json)).deep_symbolize_keys
+    result = field_partial_config(params.require(:field_type), data)
+    return head :bad_request unless result
+
+    render partial: result[:partial], locals: result[:locals]
+  rescue JSON::ParserError, Cocina::Models::ValidationError, Dry::Struct::Error
+    head :unprocessable_content
   end
 
   def update
@@ -63,6 +110,25 @@ class DescriptionsController < ApplicationController
     close_version!
     redirect_to object_path(druid: @cocina_object.externalIdentifier), flash: { success: 'Description updated.' }
   end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def field_partial_config(field_type, data)
+    case field_type
+    when 'title'       then { partial: 'descriptions/title_fields',
+                              locals: { title: Cocina::Models::Title.new(data) } }
+    when 'note'        then { partial: 'descriptions/note_fields',
+                              locals: { note: Cocina::Models::DescriptiveValue.new(data) } }
+    when 'language'    then { partial: 'descriptions/language_fields',
+                              locals: { language: Cocina::Models::Language.new(data) } }
+    when 'contributor' then { partial: 'descriptions/contributor_fields', locals: { contributor: data } }
+    when 'subject'     then { partial: 'descriptions/subject_fields',     locals: { subject: data } }
+    when 'form'        then { partial: 'descriptions/form_fields',         locals: { form: data } }
+    when 'event'       then { partial: 'descriptions/event_fields',        locals: { event: data } }
+    when 'related_resource' then { partial: 'descriptions/related_resource_fields',
+                                   locals: { related_resource: data } }
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def build_description
     DescriptionBuilder.new(existing_description: @cocina_object.description.to_h)
--- app/javascript/controllers/json_formatter_controller.js
+++ app/javascript/controllers/json_formatter_controller.js
@@ -1,21 +1,29 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  #collapse = null
+  #onShow = () => { this.element.disabled = false }
+  #onShown = () => { this.resize() }
+  #onHide = () => { this.element.disabled = true }
+
   connect () {
     const collapse = this.element.closest('.collapse')
     if (collapse) {
-      this.element.disabled = true
-      collapse.addEventListener('show.bs.collapse', () => {
-        this.element.disabled = false
-      })
-      collapse.addEventListener('shown.bs.collapse', () => {
-        this.resize()
-      })
-      collapse.addEventListener('hide.bs.collapse', () => {
-        this.element.disabled = true
-      })
+      this.element.disabled = !collapse.classList.contains('show')
+      collapse.addEventListener('show.bs.collapse', this.#onShow)
+      collapse.addEventListener('shown.bs.collapse', this.#onShown)
+      collapse.addEventListener('hide.bs.collapse', this.#onHide)
+      this.#collapse = collapse
     }
     this.resize()
+  }
+
+  disconnect () {
+    if (this.#collapse) {
+      this.#collapse.removeEventListener('show.bs.collapse', this.#onShow)
+      this.#collapse.removeEventListener('shown.bs.collapse', this.#onShown)
+      this.#collapse.removeEventListener('hide.bs.collapse', this.#onHide)
+    }
   }
 
   resize () {
@@ -29,8 +37,52 @@ export default class extends Controller {
       this.element.value = JSON.stringify(parsed, null, 2)
       this.element.classList.remove('is-invalid')
       this.resize()
+      this.syncToForm()
     } catch {
       this.element.classList.add('is-invalid')
     }
+  }
+
+  syncToForm () {
+    // Only sync json_toggle textareas (inside .collapse), not the "must edit as JSON" fallback ones
+    const collapse = this.element.closest('.collapse')
+    if (!collapse) return
+
+    const dataItem = this.element.closest('[data-item]')
+    if (!dataItem) return
+
+    const fieldType = dataItem.dataset.fieldType
+    if (!fieldType) return
+
+    const urlContainer = this.element.closest('[data-render-field-url]')
+    if (!urlContainer) return
+
+    const url = urlContainer.dataset.renderFieldUrl
+    const csrfToken = document.querySelector('meta[name="csrf-token"]')?.content
+
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'X-CSRF-Token': csrfToken },
+      body: JSON.stringify({ field_type: fieldType, json: this.element.value })
+    })
+      .then(response => response.ok ? response.text() : null)
+      .then(html => {
+        if (!html) return
+        const temp = document.createElement('div')
+        temp.innerHTML = html
+        const newItem = temp.firstElementChild
+        if (!newItem) return
+
+        // Re-open the collapse so the user can keep editing
+        const newCollapse = newItem.querySelector('.collapse')
+        if (newCollapse) {
+          newCollapse.classList.add('show')
+          const toggleBtn = newItem.querySelector('[data-bs-toggle="collapse"]')
+          if (toggleBtn) toggleBtn.setAttribute('aria-expanded', 'true')
+        }
+
+        dataItem.replaceWith(newItem)
+      })
+      .catch(() => {})
   }
 }
