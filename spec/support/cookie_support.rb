240359006481391afb3c862d94478bb9f719a982
Justin Littman <justinlittman@stanford.edu>
2026-05-18 13:19:30 -0400

    Handle bulk action forms that do not have source properties.

--- spec/support/cookie_support.rb
+++ spec/support/cookie_support.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-def set_last_search_cookie(search_form: SearchForm.new(query: 'test', page: 5), total_results: 10) # rubocop:disable Metrics/AbcSize
+def signed_last_search_cookie(search_form: SearchForm.new(query: 'test', page: 5), total_results: 10)
+  Rails.application
+       .message_verifier(Rails.application.config.action_dispatch.signed_cookie_salt)
+       .generate({ form: search_form.attributes, total_results: })
+end
+
+def set_last_search_cookie(search_form: SearchForm.new(query: 'test', page: 5), total_results: 10)
   visit root_path
-  signed_value = Rails.application
-                      .message_verifier(Rails.application.config.action_dispatch.signed_cookie_salt)
-                      .generate({
-                                  form: search_form.attributes, total_results:
-                                })
+  signed_value = signed_last_search_cookie(search_form:, total_results:)
   page.driver.browser.manage.add_cookie(
     name: :last_search,
     value: signed_value,
