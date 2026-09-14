bdc6bbf91584ef2f12ea820a0634b0d3abca9ea6
Peter Mangiafico <pmangiafico@stanford.edu>
2026-09-14 15:50:22 -0700

    impersonation UI changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,6 +10,8 @@ class AdminController < ApplicationController
 
   def impersonate
     authorize! :impersonate?, with: AdminPolicy
+    deny_access if Current.impersonating?
+
     available_workgroups = Impersonation::Workgroups.available_for_user(user: current_user)
 
     @selected_workgroups = Current.impersonated_groups || []
@@ -18,20 +20,23 @@ class AdminController < ApplicationController
 
   def update_impersonation
     authorize! :impersonate?, with: AdminPolicy
-    selected_workgroups = impersonation_params[:workgroups] || []
-    available_workgroups = Impersonation::Workgroups.available_for_user(user: current_user)
-    impersonated_workgroups = Array(selected_workgroups) & available_workgroups
 
-    Impersonation::Workgroups.update_cookie(cookies:, groups: impersonated_workgroups)
-
-    redirect_to admin_impersonate_path
+    if impersonated_workgroups.empty?
+      flash[:warning] = 'You must select workgroups to enable impersonation.'
+      redirect_to admin_impersonate_path
+    else
+      Impersonation::Workgroups.update_cookie(cookies:, groups: impersonated_workgroups)
+      flash[:success] = 'You are now impersonating the selected workgroups.'
+      redirect_to root_path
+    end
   end
 
   def stop_impersonating
     authorize! :stop_impersonating?, with: AdminPolicy
     Impersonation::Workgroups.clear_cookie(cookies:)
 
-    redirect_to admin_impersonate_path
+    flash[:success] = 'You have stopped impersonating.'
+    redirect_to root_path
   end
 
   private
@@ -40,5 +45,11 @@ class AdminController < ApplicationController
     params.expect(impersonation: [{ workgroups: [] }])
   rescue ActionController::ParameterMissing
     ActionController::Parameters.new
+  end
+
+  def impersonated_workgroups
+    selected_workgroups = impersonation_params[:workgroups] || []
+    available_workgroups = Impersonation::Workgroups.available_for_user(user: current_user)
+    Array(selected_workgroups) & available_workgroups
   end
 end
