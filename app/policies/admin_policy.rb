836e669126228ab48324d49f3af6926b884670b6
Peter Mangiafico <pmangiafico@stanford.edu>
2026-09-15 09:24:10 -0700

    update admin policy, move strings to locale

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -4,11 +4,17 @@
 class AdminPolicy < ApplicationPolicy
   alias_rule :manage_permissions?, :groups?, to: :admin?
 
+  # can only start/update impersonating if an admin and not currently impersonating
   def impersonate?
+    admin? || !Current.impersonating?
+  end
+
+  # can only stop impersonating if an admin and currently impersonating
+  def stop_impersonating?
     admin? || Current.impersonating?
   end
 
-  alias_rule :update_impersonation?, :stop_impersonating?, to: :impersonate?
+  alias_rule :update_impersonation?, to: :impersonate?
 
   # NOTE: Allowing admins is handled by precheck in ApplicationPolicy so returning false still allows admins.
 end
