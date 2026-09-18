df87623bc43a22067a4f66347865e2350187000d
Laura Wrubel <lwrubel@stanford.edu>
2026-09-18 15:46:57 -0400

    Send SDR events when permissions created or destroyed

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -5,6 +5,9 @@
 #
 # Note that there is a default admin permission for sdr:argo_administrators workgroup.
 class Permission < ApplicationRecord
+  CREATED_EVENT_TYPE = 'argo_permission_created'
+  DELETED_EVENT_TYPE = 'argo_permission_deleted'
+
   validates :workgroup, presence: true
 
   enum :permission_type, {
@@ -31,6 +34,9 @@ class Permission < ApplicationRecord
   validates :target_druid, druid: true, allow_nil: true
   validate :target_druid_present_for_restricted_permission_types
 
+  after_create_commit :create_created_event
+  after_destroy_commit :create_deleted_event
+
   private
 
   def target_druid_present_for_restricted_permission_types
@@ -38,5 +44,29 @@ class Permission < ApplicationRecord
     return unless permission_type_read_restricted? || permission_type_edit?
 
     errors.add(:target_druid, "can't be blank for #{permission_type} permission type")
+  end
+
+  def create_created_event
+    create_event(type: CREATED_EVENT_TYPE)
+  end
+
+  def create_deleted_event
+    create_event(type: DELETED_EVENT_TYPE)
+  end
+
+  # Record the permission change as an event on the target object in SDR.
+  # Events are only created when there is a target object and a current user,
+  # e.g., not for permissions created from the console, seeds, or a data migration.
+  # A failure to create the event must not prevent the permission change itself.
+  # @param [String] type the type of the event
+  def create_event(type:)
+    return if target_druid.blank? || Current.user.blank?
+
+    Sdr::Event.create(druid: target_druid, type:,
+                      data: { who: Current.user.sunetid,
+                              host: Sdr::Event.host,
+                              permission_type: })
+  rescue StandardError => e
+    Honeybadger.notify(e, context: { permission_id: id, target_druid:, event_type: type })
   end
 end
